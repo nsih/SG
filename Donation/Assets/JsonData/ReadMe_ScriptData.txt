10f3dd2d81138cfddcab00f3e3096b50630fb828
sih <81147612+nsih@users.noreply.github.com>
2022-11-08 23:20:05 +0900

    Revert "Sih branch"

--- Donation/Assets/JsonData/ReadMe_ScriptData.txt
+++
@@ -1,6 +0,0 @@
-식별자		"id": 1,
-활성	           	"isPlayer": "true",
-대사	            "playerScript": "대사",
-표정	            "playerEmote": "default",
-도내창활성           "isDonation": "true",
-도내창대사           "donationScript": ""
--- Donation/Assets/Script/Battle/Attack.cs
+++ Donation/Assets/Script/Battle/Attack.cs
@@ -4,11 +4,87 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collider) 
+    float cooltime;
+    float curtime;
+    public GameObject PlayerManager;
+    private HashSet<Enemy> list = null;
+    // Start is called before the first frame update
+    void Awake()
     {
-        if (collider.tag == "Enemy")
+        cooltime = PlayerManager.GetComponent<PlayerCon>().cooltime;
+        this.list = new HashSet<Enemy>();
+    }
+    
+    void FixedUpdate()
+    {
+        curtime -= Time.deltaTime;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.transform.GetComponent<Enemy>();
+        if (enemy != null && enemy.tag == "Enemy")
         {
-            collider.gameObject.GetComponent<Enemy>().hp--;
+            //Debug.Log("AddList name" + enemy);
+            this.list.Add(enemy);
         }
     }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Enemy enemy = collision.transform.GetComponent<Enemy>();
+        if(enemy != null && this.list.Contains(enemy) == true)
+        {
+            //Debug.Log("RemoveList name : " + enemy.name);
+            this.list.Remove(enemy);
+        }
+    }
+
+    public void AttackEnemy(HashSet<Enemy> enemylist)
+    {
+        if (PlayerManager.GetComponent<PlayerCon>().check == false && curtime <= 0)
+        {
+            curtime = cooltime;
+            foreach(Enemy enemy in enemylist)
+            {
+                if (!enemy.attackCheck)
+                {
+                    //Debug.Log("Attacked name : " + enemy.name);
+                    enemy.attackCheck = true;
+                    enemy.Attack();
+                    enemy.hp--;
+                }
+            }
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        /*
+        //Collider2D[] mobs = Physics2D.OverlapCollider(Collider2D collision, ContactFilter2D cF, Collider2D[] result)
+        //for(int i = 0;i< collision.Length; i++)
+        {
+            if (collision.tag == "Enemy")
+            {
+                //Debug.Log(collision.Length);
+                if (PlayerManager.GetComponent<PlayerCon>().check == false && curtime <= 0)
+                {
+                    curtime = cooltime;
+                    if (!collision.gameObject.GetComponent<Enemy>().attackCheck)
+                    {
+                        collision.gameObject.GetComponent<Enemy>().attackCheck = true;
+                        collision.gameObject.GetComponent<Enemy>().Attack();
+                        collision.gameObject.GetComponent<Enemy>().hp--;
+                    }
+                }
+            }
+        }
+        */
+        Enemy enemy = collision.transform.GetComponent<Enemy>();
+        if (list != null)
+        {
+            AttackEnemy(list);
+        }
+    }
+
+   
 }
